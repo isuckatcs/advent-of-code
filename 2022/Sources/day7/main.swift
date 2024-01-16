c494f524de49a62b1d68afc5f0b20a90561cd48f
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2024-01-16 19:41:10 +0100

    day7

--- 2022/Sources/day7/main.swift
+++ 2022/Sources/day7/main.swift
@@ -1,28 +1,8 @@
-protocol Node {
-    var name: String { get }
-    var parent: Directory? { get }
-
-    func size() -> UInt64
-}
-
-class File : Node {
-    let name: String
-    let fileSize: UInt64
-    let parent: Directory?
-
-    init(name: String, size: UInt64, parent: Directory) {
-        self.name = name
-        self.fileSize = size
-        self.parent = parent
-    }
-
-    func size() -> UInt64 { fileSize }
-}
-
-class Directory : Node {
+class Directory {
     let name: String
     let parent: Directory?
-    var files: [File] = []
+
+    var fileSize: UInt64 = 0
     var subdirs: [Directory] = []
 
     init(name: String, parent: Directory?) {
@@ -31,14 +11,13 @@ class Directory : Node {
     }
 
     func size() -> UInt64 { 
-        subdirs.reduce(0, { $0 + $1.size() })
-        + files.reduce(0, { $0 + $1.size() }) 
+        subdirs.reduce(0, { $0 + $1.size() }) + fileSize 
     }
 }
 
 func parseFileSystem(_ commands: [String]) -> Directory {
     let root = Directory(name: "/", parent: nil)
-
+    
     var curDir = root
     for command in commands {
         let split = command.split(separator: " ")
@@ -52,50 +31,34 @@ func parseFileSystem(_ commands: [String]) -> Directory {
                     curDir = curDir.subdirs.first { $0.name == split[2] } ?? curDir
                 }
             }
-            break
         case "dir":
             curDir.subdirs.append(Directory(name: String(split[1]), parent: curDir))
         default:
-            curDir.files.append(File(name: String(split[1]), size: UInt64(split[0])!, parent: curDir))
+            curDir.fileSize += UInt64(split[0])!
         }
     }
 
     return root
 }
 
-func part1(_ node: Node) -> UInt64 {
-    var size: UInt64 = 0
+func part1(_ dir: Directory) -> UInt64 {
+    var size: UInt64 = dir.size() < 100000 ? dir.size() : 0
     
-    if let dir = node as? Directory {
-        let dsize = dir.size()
-        if(dsize < 100000) {
-            size += dsize
-        }
-
-        for subdir in dir.subdirs {
-            size += part1(subdir)
-        }
-
+    for subdir in dir.subdirs {
+        size += part1(subdir)
     }
 
     return size
 }
 
-func part2(_ node: Node, _ limit: UInt64) -> UInt64 {
-    var m: UInt64 = 70000000
+func part2(_ dir: Directory, _ limit: UInt64) -> UInt64 {
+    var size: UInt64 = dir.size() > limit ? dir.size() : 70000000
 
-    if let dir = node as? Directory {
-        let dsize = dir.size()
-        if(dsize >= limit) {
-            m = min(dsize, m)
-        }
-
-        for subdir in dir.subdirs {
-            m = min(m, part2(subdir, limit))
-        }
+    for subdir in dir.subdirs {
+        size = min(size, part2(subdir, limit))
     }
 
-    return m
+    return size
 }
 
 var lines: [String] = [String]()
