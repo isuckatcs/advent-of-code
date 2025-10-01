6de17c75abaee5d5d53d4f2e2558a25bfccc034b
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2025-10-01 23:18:30 +0200

    day1

---
+++ 2018/day1/Program.cs
@@ -0,0 +1,29 @@
+﻿int part1(List<int> input)
+{
+    return input.Sum();
+}
+
+int part2(List<int> input)
+{
+    int freq = 0;
+    int idx = 0;
+    var seen = new HashSet<int>();
+
+    while (true)
+    {
+        freq += input[idx];
+        if (!seen.Add(freq))
+            return freq;
+
+        idx = (idx + 1) % input.Count;
+    }
+}
+
+var input = new List<int>();
+
+string? line;
+while ((line = Console.ReadLine()) != null)
+    input.Add(int.Parse(line));
+
+Console.WriteLine(part1(input));
+Console.WriteLine(part2(input));
